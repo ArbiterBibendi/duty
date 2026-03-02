a627f6447a9c8d335c71a38867e610e7696e67ba
Drake Scott <ArbiterBibendi@yahoo.com>
2026-03-02 16:06:09 -0600

    init express

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+	if (error) throw error;
+	console.log("Express server running!");
+});
